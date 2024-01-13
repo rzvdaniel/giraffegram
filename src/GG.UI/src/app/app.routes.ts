80f023660f24d8bc92cb5a2afcd1129927f7c2e3
rzvdaniel <rzvdaniel@gmail.com>
2024-01-13 23:25:23 +0100

    Added home page and default dashboard

--- src/GG.UI/src/app/app.routes.ts
+++ src/GG.UI/src/app/app.routes.ts
@@ -9,7 +9,60 @@ import { LayoutComponent } from 'app/layout/layout.component';
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 export const appRoutes: Route[] = [
 
-    {path: '', pathMatch : 'full', redirectTo: 'coming-soon/fullscreen'},
+    // Redirect empty path to '/dashboards/project'
+    {path: '', pathMatch : 'full', redirectTo: 'home'},
+
+    // Redirect signed-in user to the '/dashboards/project'
+    //
+    // After the user signs in, the sign-in page will redirect the user to the 'signed-in-redirect'
+    // path. Below is another redirection for that path to redirect the user to the desired
+    // location. This is a small convenience to keep all main routes together here on this file.
+    {path: 'signed-in-redirect', pathMatch : 'full', redirectTo: 'dashboards/project'},
+
+    // Auth routes for guests
+    {
+        path: '',
+        canActivate: [NoAuthGuard],
+        canActivateChild: [NoAuthGuard],
+        component: LayoutComponent,
+        data: {
+            layout: 'empty'
+        },
+        children: [
+            {path: 'confirmation-required', loadChildren: () => import('app/modules/auth/confirmation-required/confirmation-required.routes')},
+            {path: 'forgot-password', loadChildren: () => import('app/modules/auth/forgot-password/forgot-password.routes')},
+            {path: 'reset-password', loadChildren: () => import('app/modules/auth/reset-password/reset-password.routes')},
+            {path: 'sign-in', loadChildren: () => import('app/modules/auth/sign-in/sign-in.routes')},
+            {path: 'sign-up', loadChildren: () => import('app/modules/auth/sign-up/sign-up.routes')}
+        ]
+    },
+    
+    // Auth routes for authenticated users
+    {
+        path: '',
+        canActivate: [AuthGuard],
+        canActivateChild: [AuthGuard],
+        component: LayoutComponent,
+        data: {
+            layout: 'empty'
+        },
+        children: [
+            {path: 'sign-out', loadChildren: () => import('app/modules/auth/sign-out/sign-out.routes')},
+            {path: 'unlock-session', loadChildren: () => import('app/modules/auth/unlock-session/unlock-session.routes')}
+        ]
+    },
+
+     // Landing routes
+     {
+        path: '',
+        component: LayoutComponent,
+        data: {
+            layout: 'empty'
+        },
+        children: [
+            {path: 'home', loadChildren: () => import('app/modules/landing/home/home.routes')},
+        ]
+    },
 
     // Maintenance routes
     {
@@ -25,7 +78,29 @@ export const appRoutes: Route[] = [
         ]   
     },
 
+    // Admin routes
+    {
+        path: '',
+        canActivate: [AuthGuard],
+        canActivateChild: [AuthGuard],
+        component: LayoutComponent,
+        resolve: {
+            initialData: initialDataResolver
+        },
+        children: [
+
+            // Dashboards
+            {path: 'dashboards', children: [
+                {path: 'analytics', loadChildren: () => import('app/modules/admin/dashboards/analytics/analytics.routes')},
+            ]},
+
+            // 404 & Catch all
+            {path: '404-not-found', pathMatch: 'full', loadChildren: () => import('app/modules/admin/pages/error/error-404/error-404.routes')},
+            {path: '**', redirectTo: '404-not-found'}
+        ]
+    },
+
     // 404 & Catch all
-    {path: '404-not-found', pathMatch: 'full', loadChildren: () => import('app/modules/admin/pages/coming-soon/coming-soon.routes')},
-    {path: '**', pathMatch : 'full', redirectTo: 'coming-soon/fullscreen'}
+    {path: '404-not-found', pathMatch: 'full', loadChildren: () => import('app/modules/admin/pages/error/error-404/error-404.routes')},
+    {path: '**', pathMatch : 'full', redirectTo: '404-not-found'}
 ];
