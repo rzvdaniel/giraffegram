28088ca7d192de2330d4c907adc6ce1acf2255e5
rzvdaniel <rzvdaniel@gmail.com>
2024-02-01 00:08:35 +0100

    Updated home

--- src/GG.UI/src/app/modules/landing/home/home.component.ts
+++ src/GG.UI/src/app/modules/landing/home/home.component.ts
@@ -8,7 +8,7 @@ import { RouterLink } from '@angular/router';
     templateUrl  : './home.component.html',
     encapsulation: ViewEncapsulation.None,
     standalone   : true,
-    imports      : [MatButtonModule, RouterLink, MatIconModule],
+    imports      : [MatButtonModule, RouterLink, MatIconModule ],
 })
 export class LandingHomeComponent
 {
