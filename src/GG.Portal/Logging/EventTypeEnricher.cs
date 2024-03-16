cd314e25785c181651bd1d3783433f7a0c15a8be
rzvdaniel <rzvdaniel@gmail.com>
2024-03-17 00:08:24 +0100

    Added serilog packages

---
+++ src/GG.Portal/Logging/EventTypeEnricher.cs
@@ -0,0 +1,18 @@
+using Murmur;
+using Serilog.Core;
+using System.Text;
+
+namespace GG.Portal.Logging;
+
+public class EventTypeEnricher : ILogEventEnricher
+{
+    public void Enrich(Serilog.Events.LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+    {
+        var murmur = MurmurHash.Create32();
+        var bytes = Encoding.UTF8.GetBytes(logEvent.MessageTemplate.Text);
+        var hash = murmur.ComputeHash(bytes);
+        var numericHash = BitConverter.ToUInt32(hash, 0);
+        var eventId = propertyFactory.CreateProperty("EventType", numericHash);
+        logEvent.AddPropertyIfAbsent(eventId);
+    }
+}
---
+++ src/GG.Portal/Logging/UserNameEnricher.cs
@@ -0,0 +1,27 @@
+using Serilog.Core;
+
+namespace GG.Portal.Logging;
+
+public class UserNameEnricher : ILogEventEnricher
+{
+    readonly IHttpContextAccessor _httpContextAccessor;
+
+    public UserNameEnricher(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public void Enrich(Serilog.Events.LogEvent logEvent, ILogEventPropertyFactory factory)
+    {
+        if (!(_httpContextAccessor.HttpContext?.User.Identity!.IsAuthenticated ?? false))
+        {
+            return;
+        }
+
+        var user = _httpContextAccessor.HttpContext.User;
+        var userName = user.Identity.Name;
+
+        var userNameProperty = factory.CreateProperty("UserName", userName);
+        logEvent.AddOrUpdateProperty(userNameProperty);
+    }
+}
